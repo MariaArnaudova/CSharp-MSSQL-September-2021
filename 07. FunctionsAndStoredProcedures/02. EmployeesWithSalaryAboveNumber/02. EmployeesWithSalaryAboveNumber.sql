eba87ce819e26690bebdcfd32df3e2b20c4a069d
MariaCode <m.p.arnaudova@gmail.com>
2024-04-21 22:11:42 +0300

    Added solution

---
+++ 07. FunctionsAndStoredProcedures/02. EmployeesWithSalaryAboveNumber/02. EmployeesWithSalaryAboveNumber.sql
@@ -0,0 +1,16 @@
+CREATE  PROC usp_GetEmployeesSalaryAboveNumber (@number DECIMAL(18,4) )
+	     AS
+     SELECT FirstName, LastName
+       FROM Employees
+      WHERE Salary >= @number
+GO
+
+--EXEC usp_GetEmployeesSalaryAboveNumber 48100
+
+
+--ALTER PROC usp_GetEmployeesSalaryAboveNumber (@number DECIMAL(18,4)) 
+--	     AS
+--     SELECT FirstName, LastName
+--       FROM Employees
+--      WHERE Salary >= @number
+--GO
